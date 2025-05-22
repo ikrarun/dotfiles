e3cf6ecfb9befce0ed8a8e737b08182cb0ecd9c6
Arun <thewiseconcern@gmail.com>
2025-05-22 22:24:44 +0530

    Initial Commit

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2025-05-22 05:25:03
+set PATH $PATH /home/arun/.local/bin
---
+++ hypr/.config/hypr/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Wait a bit for Hyprland to fully start
+sleep 3
+
+# Show a popup notification (you can customize title & message)
+notify-send "Welcome to Hyprland" "Have a productive session, Suraj! 🌟"
+
