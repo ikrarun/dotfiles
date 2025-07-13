78800be217a2f4d6739dae5ddab2c630fe015dc1
ikrarun <thewiseconcern@gmail.com>
2025-07-13 13:50:16 +0530

    Some better changes

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+clear
+ufetch  
+echo "🔥 Welcome back, $USER — let's crush some code! 🔥"
+
 # Created by `pipx` on 2025-05-22 05:25:03
 set PATH $PATH /home/arun/.local/bin
 
